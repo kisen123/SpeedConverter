50668ccbe9f3ca64511fbd58c535f082aebc88a9
kisen123 <molbachlian@gmail.com>
2024-06-06 22:41:56 +0200

    Clean-up after removing the clickable box

--- script.js
+++ script.js
@@ -1,45 +1,4 @@
-let speed = document.querySelector("basic-input-box");
-console.log(speed);
-
-
-
-
-
-
-// This function calculates the speed inside the basic input box
-/*
-function calculateSpeed() {
-    outputHTML = document.querySelector("basic-box-result");
-    console.log(outputHTML)
-    output.innerHTML = "RESULT"
-};
-*/
-
-function showPaceAfterClick(minutes, seconds) {
-
-    // Rewriting to double digits
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-
-    document.getElementById("basic-box-result-minutes").textContent = minutes;
-    document.getElementById("basic-box-result-seconds").textContent = seconds;
-}
-
-
-function showPaceAfterRangeChange(minutes, seconds) {
-
-    // Rewriting to double digits
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-
-    document.getElementById("basic-range-result-minutes").textContent = minutes;
-    document.getElementById("basic-range-result-seconds").textContent = seconds;
-}
-
-
-
+// Updates the the input when the range slider is adjusted
 function updateInputFromRange(input) {
     document.getElementById('basic-input-speed-range').value = input;
 
@@ -59,29 +18,16 @@ function updateInputFromRange(input) {
 }
 
 
+function showPaceAfterRangeChange(minutes, seconds) {
+
+    // Rewriting to double digits
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
 
 
-
-// Function to handle button click .
-function handleCalculateSpeedClick() {
-    
-    // Collects the input value from the basic-box
-    var inputHTMLObject = document.getElementById("basic-input-speed-box");
-
-
-    // Calculating the pace from the speed
-    pace = calculateSpeed(inputHTMLObject.value);
-    
-    // Logging the output to the console for debugging purposes
-    // console.log("The calculated pace is: " + pace.minutes + ":" + pace.seconds);
-
-
-    // Showing the pace after the user clicks the button
-    showPaceAfterClick(pace.minutes, pace.seconds);
-
-};
-
-
+    document.getElementById("basic-range-result-minutes").textContent = minutes;
+    document.getElementById("basic-range-result-seconds").textContent = seconds;
+}
 
 
 
@@ -115,9 +61,6 @@ function calculateSpeed(inputValue) {
     // console.log("Seconds: " + seconds)
     return {minutes, seconds}
 };
-
-// Adds event listener to the 'calculate result' button
-// document.getElementById('basic-box-submit-number').addEventListener('click', handleCalculateSpeedClick);
 
 // Adds event listener to the range input box.
 
