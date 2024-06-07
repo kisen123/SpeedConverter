ec645ace75940fff82c9a743b7b4ec7de47f3f7e
kisen123 <molbachlian@gmail.com>
2024-06-07 19:55:14 +0200

    Added projected times results for 5km, 10km, 21.1km and 42.2km

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ function updateInputFromRange(input) {
     document.getElementById('basic-input-speed-range').value = input;
 
     // Collects the input value from the basic-box
-    var inputHTMLObject = document.getElementById("basic-input-speed-range");
+    let speed = document.getElementById("basic-input-speed-range").value;
 
 
     // Calculating the pace from the speed
-    pace = calculateSpeed(inputHTMLObject.value);
+    pace = calculateSpeed(speed);
     
     // Uncomment to log the output to the console for debugging purposes
     // console.log("The calculated pace is: " + pace.minutes + ":" + pace.seconds);
@@ -15,8 +15,42 @@ function updateInputFromRange(input) {
 
     // Showing the pace after the user clicks the button
     showPaceAfterRangeChange(pace.minutes, pace.seconds);
+
+    // Updating the projected times section
+    updateAvgProjectedTime(speed)
 }
 
+function updateAvgProjectedTime(selectedSpeed) {
+    //let selectedSpeed = document.getElementById('basic-input-speed-range').value;
+
+    // Looping over the distances, and calculating the average projected time from the speed and distance
+    let listOfKMDistances = ['5km', '10km', '21.1km', '42.2km'];
+
+    for (const kmDistance of listOfKMDistances) {
+
+        avgProjectedTime = calculateAvgProjectedTime(selectedSpeed, kmDistance);
+
+        showAvgProjectedTimeAfterRangeChange(avgProjectedTime.minutes, avgProjectedTime.seconds, kmDistance)
+
+    }
+
+}
+
+
+function showAvgProjectedTimeAfterRangeChange(minutes, seconds, kmDistance) {
+
+    // Rewriting to double digits
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    let HTMLElementMinutes = `avg-pace-${kmDistance}-projected-time-result-minutes`;
+    let HTMLElementSeconds = `avg-pace-${kmDistance}-projected-time-result-seconds`;
+
+
+    document.getElementById(HTMLElementMinutes).textContent = minutes;
+    document.getElementById(HTMLElementSeconds).textContent = seconds;
+
+}
 
 function showPaceAfterRangeChange(minutes, seconds) {
 
@@ -54,7 +88,6 @@ function calculateSpeed(inputValue) {
 
     // Handles situations where the rounding
     // protocol gives f.ex 04:60 instead of 05:00
-    
     if (seconds === 60) {
         minutes = minutes + 1;
         seconds = 0;
@@ -65,6 +98,38 @@ function calculateSpeed(inputValue) {
     // console.log("Seconds: " + seconds)
     return {minutes, seconds}
 };
+
+// Function to calculate average projected time for 
+function calculateAvgProjectedTime(speed, kmDistance) {
+
+    let rawConvertedMinutesAvgProjected;
+
+    // Rewriting the km values to integers
+    kmDistance = parseFloat(kmDistance.replace('km', ''));
+
+    // Defining minutes and seconds
+    if (speed === 0) {
+        rawConvertedMinutesAvgProjected = 0;
+    } else {
+    rawConvertedMinutesAvgProjected = (kmDistance / speed) * 60;
+    }
+
+
+    let minutes = Math.floor(rawConvertedMinutesAvgProjected);
+    let seconds = Math.round((rawConvertedMinutesAvgProjected % 1) * 60, 2);
+
+    // Handles situations where the rounding
+    // protocol gives f.ex 04:60 instead of 05:00
+    if (seconds === 60) {
+        minutes = minutes + 1;
+        seconds = 0;
+    }
+    
+    // Uncomment for debugging
+    // console.log("Minutes: " + minutes);
+    // console.log("Seconds: " + seconds)
+    return {minutes, seconds}
+}
 
 // Adds event listener to the range input box.
 
